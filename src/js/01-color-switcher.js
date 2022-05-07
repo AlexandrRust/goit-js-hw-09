0d841067e5c49ef9ec64609fec4d08ffae1a733f
AlexandrRust <123_00@ukr.net>
2022-05-07 11:03:42 +0300

    hw-js-09-1-tasck

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+}
 
+let styleColor = null;
+
+function colorStyle() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+refs.btnStart.addEventListener('click', () => {
+    styleColor = setInterval(colorStyle, 1000);
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+});
+    
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(styleColor);
+    refs.btnStart.disabled = false;
+    refs.btnStop.disabled = true;
+})
+    
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
